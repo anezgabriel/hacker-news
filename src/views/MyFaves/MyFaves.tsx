1b3b8604364cc11c611afff7d8b40c211f7e4fe7
Gabriel Añez <artu242@gmail.com>
2021-09-03 17:35:31 -0400

    added My favorites feature

--- src/views/MyFaves/MyFaves.tsx
+++ src/views/MyFaves/MyFaves.tsx
@@ -1,9 +1,19 @@
+import { useAppSelector } from '../../app/hooks';
+import PostsGrid from '../../components/PostsGrid/PostsGrid';
+import { selectFavorites } from '../../features/persisted/persistedSlice';
 import classes from './MyFaves.module.css';
 
 const MyFaves: React.FC = () => {
+  const favorites = useAppSelector(selectFavorites);
+
   return (
     <div className={classes.container}>
-      <div>My faves</div>
+      {favorites && favorites.length === 0 && (
+        <div className={classes.message}>
+          <h1>You have no favorites :(</h1>
+        </div>
+      )}
+      {favorites && favorites.length > 0 && <PostsGrid data={favorites} />}
     </div>
   );
 };
