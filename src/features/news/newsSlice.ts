e67f719f32869007bb782f13139d45b22f6cf2bf
Gabriel Añez <artu242@gmail.com>
2021-09-02 21:41:03 -0400

    Added axios and fetchData reducers

--- src/features/news/newsSlice.ts
+++ src/features/news/newsSlice.ts
@@ -1,15 +1,39 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { RootState } from '../../app/store';
+import axios from 'axios';
 
+interface searchParams {
+  query: string;
+  page: number;
+}
 export interface NewsState {
   data: any[];
   toggle: string;
+  loading: boolean;
+  error: boolean;
 }
 
 const initialState: NewsState = {
   data: [],
   toggle: 'All',
+  loading: false,
+  error: false,
 };
+
+export const fetchData = createAsyncThunk(
+  'news/fetchData',
+  async (searchParams: searchParams) => {
+    const { query, page } = searchParams;
+    try {
+      const response = await axios.get(
+        `https://hn.algolia.com/api/v1/search_by_date?hitsPerPage=8&query=${query}&page=${page}`
+      );
+      return response.data.hits;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
 export const newsSlice = createSlice({
   name: 'news',
@@ -19,11 +43,27 @@ export const newsSlice = createSlice({
       state.toggle = action.payload;
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(fetchData.pending, (state, action) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchData.fulfilled, (state, action) => {
+      const news = [...state.data, ...action.payload];
+      state.loading = false;
+      state.data = news;
+    });
+    builder.addCase(fetchData.rejected, (state, action) => {
+      state.loading = false;
+      state.error = true;
+    });
+  },
 });
 
 export const { changeToggle } = newsSlice.actions;
 
 export const selectNews = (state: RootState) => state.news.data;
 export const selectToggle = (state: RootState) => state.news.toggle;
+export const selectLoading = (state: RootState) => state.news.loading;
+export const selectError = (state: RootState) => state.news.error;
 
 export default newsSlice.reducer;
--- src/views/AllPosts/AllPosts.tsx
+++ src/views/AllPosts/AllPosts.tsx
@@ -1,7 +1,21 @@
+import { useEffect } from 'react';
 import classes from './AllPosts.module.css';
 import Select from '../../components/Select/Select';
+import { useAppDispatch } from '../../app/hooks';
+import { fetchData } from '../../features/news/newsSlice';
+
+const defaultParams = {
+  query: '',
+  page: 0,
+};
 
 const AllPosts = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchData(defaultParams));
+  }, [dispatch]);
+
   return (
     <div className={classes.container}>
       <Select />
